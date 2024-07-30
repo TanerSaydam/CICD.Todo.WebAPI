f2d8578aa9d6f49bf25081465a158fd080368704
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2024-07-30 21:42:39 +0300

    swagger ekledin ve endpoint ekledim

--- Program.cs
+++ Program.cs
@@ -1,7 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
 var app = builder.Build();
 
-//Note
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/getall", () =>
+{
+    List<string> todos = new()
+    {
+        "Deneme 1",
+        "Deneme 2",
+        "Deneme 3"
+    };
+
+    return Results.Ok(todos);
+});
 
 app.Run();
